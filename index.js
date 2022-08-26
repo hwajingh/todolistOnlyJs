d5b28ab67fe4bac5dd1ecf2f92caf739c18caf0e
hwajingh <51863703+hwajingh@users.noreply.github.com>
2022-08-26 14:44:31 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,109 @@
+import { Api } from "./api/api.js";
+
+const View = (() => {
+  const domstr = {
+    todolist: "#todolist_container",
+    deletebtn: ".deletebtn",
+    inputbox: ".todolist_input",
+  };
+
+  const render = (ele, tmp) => {
+    ele.innerHTML = tmp;
+  };
+
+  const createTmp = (arr) => {
+    let tmp = "";
+    arr.forEach((todo) => {
+      tmp += `
+        <li>
+          <span>${todo.id}${todo.title}</span>
+          <button class="deletebtn" id="${todo.id}">X</button>
+        </li>
+      `;
+    });
+    return tmp;
+  };
+
+  return {
+    render,
+    domstr,
+    createTmp,
+  };
+})();
+
+// MODEL
+const Model = ((api, view) => {
+  const { getTodos, deleteTodo, addTodo } = api;
+
+  class Todo {
+    constructor(title) {
+      this.userId = 4;
+      this.title = title;
+      this.completed = false;
+    }
+  }
+
+  class State {
+    #todolist = [];
+
+    get todolist() {
+      return this.#todolist;
+    }
+
+    set todolist(newlist) {
+      this.#todolist = [...newlist];
+
+      const ulcontainer = document.querySelector(view.domstr.todolist);
+      const tmp = view.createTmp(this.#todolist);
+      view.render(ulcontainer, tmp);
+    }
+  }
+
+  return { getTodos, deleteTodo, addTodo, State, Todo };
+})(Api, View);
+
+//controller
+const Controller = ((model, view) => {
+  const state = new model.State();
+
+  const deleteTodo = () => {
+    const ulconatiner = document.querySelector(view.domstr.todolist);
+    ulconatiner.addEventListener("click", (event) => {
+      if (event.target.className === "deletebtn") {
+        state.todolist = state.todolist.filter(
+          (ele) => +ele.id !== +event.target.id
+        );
+        model.deleteTodo(event.target.id);
+      }
+    });
+  };
+
+  const addTodo = () => {
+    const inputbox = document.querySelector(view.domstr.inputbox);
+    inputbox.addEventListener("keyup", (event) => {
+      if (event.code === "Enter" && event.target.value.trim() !== "") {
+        const newtodo = new model.Todo(event.target.value.trim());
+        model.addTodo(newtodo).then((todofrombe) => {
+          state.todolist = [todofrombe, ...state.todolist];
+        });
+        event.target.value = "";
+      }
+    });
+  };
+
+  const init = () => {
+    model.getTodos().then((todos) => {
+      state.todolist = todos;
+    });
+  };
+
+  const bootstrap = () => {
+    init();
+    deleteTodo();
+    addTodo();
+  };
+
+  return { bootstrap };
+})(Model, View);
+
+Controller.bootstrap();
